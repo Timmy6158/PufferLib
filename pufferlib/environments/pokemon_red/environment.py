7673a4b6d1a98a1ef9cae14f219cee73a0bf5d00
Your Name <you@example.com>
2024-07-28 11:38:23 -0700

    My config

--- pufferlib/environments/pokemon_red/environment.py
+++ pufferlib/environments/pokemon_red/environment.py
@@ -2,30 +2,26 @@ from pdb import set_trace as T
 
 import gymnasium
 import functools
+import uuid
 
 from pokegym import Environment
-
 import pufferlib.emulation
-import pufferlib.postprocess
+from .stream_wrapper import StreamWrapper
 
 
-def env_creator(name='pokemon_red'):
+def env_creator(name="pokemon_red"):
     return functools.partial(make, name)
 
+
 def make(name, headless: bool = True, state_path=None):
-    '''Pokemon Red'''
+    """Pokemon Red"""
     env = Environment(headless=headless, state_path=state_path)
-    env = RenderWrapper(env)
-    env = pufferlib.postprocess.EpisodeStats(env)
+    env = StreamWrapper(
+        env,
+        stream_metadata={  # stream_metadata is optional
+            "user": "Mitt",  # your username
+            "color": "000000",  # color for your text :)
+            "extra": "",  # any extra text you put here will be displayed
+        },
+    )
     return pufferlib.emulation.GymnasiumPufferEnv(env=env)
-
-class RenderWrapper(gymnasium.Wrapper):
-    def __init__(self, env):
-        self.env = env
-
-    @property
-    def render_mode(self):
-        return 'rgb_array'
-
-    def render(self):
-        return self.env.screen.screen_ndarray()
---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 demo.py --env pokemon_red --vectorization multiprocessing --mode train --track
